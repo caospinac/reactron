832344126c3cc35e5a583a7875a5232e477a8e5b
Carlos A. Ospina <carlosaospinac@gmail.com>
2018-05-04 22:58:37 -0500

    Use react-bootstrap components

--- src/components/FormExample.js
+++ src/components/FormExample.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
 
 
 class FormExample extends Component {
@@ -6,34 +7,40 @@ class FormExample extends Component {
     return (
       <div>
         <form>
-          <div className="form-group">
-            <label for="exampleFormControlInput1">Email address</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-          </div>
-          <div className="form-group">
-            <label for="exampleFormControlSelect1">Example select</label>
-            <select class="form-control" id="exampleFormControlSelect1">
+          <FormGroup>
+            <ControlLabel>Email address</ControlLabel>
+            <FormControl
+              type="email" id="exampleFormControlInput1"
+              placeholder="name@example.com"
+            />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Example select</ControlLabel>
+            <FormControl componentClass="select" id="exampleFormControlSelect1">
               <option>1</option>
               <option>2</option>
               <option>3</option>
               <option>4</option>
               <option>5</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <label for="exampleFormControlSelect2">Example multiple select</label>
-            <select multiple class="form-control" id="exampleFormControlSelect2">
+            </FormControl>
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Example multiple select</ControlLabel>
+            <FormControl componentClass="select"
+              id="exampleFormControlSelect2" multiple>
               <option>1</option>
               <option>2</option>
               <option>3</option>
               <option>4</option>
               <option>5</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <label for="exampleFormControlTextarea1">Example textarea</label>
-            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-          </div>
+            </FormControl>
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Example textarea</ControlLabel>
+            <FormControl
+              componentClass="textarea" id="exampleFormControlTextarea1" rows="3"
+            />
+          </FormGroup>
         </form>
       </div>
     );
